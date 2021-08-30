878e4f981183a8a37d322a06835ea98ef3881a7a
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2021-08-31 01:38:01 +0200

    no message

--- kafka/entrypoint.sh
+++ kafka/entrypoint.sh
@@ -2,4 +2,7 @@
 
 export LOG_DIR="${APPDIR}/logs"
 
+sed "s/ZOOKEEPER_HOSTS/${ZOOKEEPER_HOSTS}/g" -i config/server.properties
+sed "s/DATA_DIR/${APPDIR}\/data/g" -i config/server.properties
+
 kafka-server-start.sh config/server.properties
