3ff86ec47fa01d99b9749961e5ce3d806270daeb
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2021-08-30 22:07:53 +0200

    no message

--- zookeeper/entrypoint.sh
+++ zookeeper/entrypoint.sh
@@ -2,6 +2,9 @@
 
 export ZOO_LOG_DIR=${APPDIR}/logs
 
-ls -lh
+echo "${ZOOKEEPER_ID}" > "${APPDIR}"/data/myid
+
+sed "s/${HOSTNAME}/0.0.0.0/g" -i "${APPDIR}/conf/zoo.cfg"
+sed "s|DATADIR|${APPDIR}|g" -i "${APPDIR}/conf/zoo.cfg"
 
 zkServer.sh --config "${APPDIR}/conf" start-foreground
