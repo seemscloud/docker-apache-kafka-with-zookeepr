d6e4b6db83d9f53c9f17550de0694f4c58b14c1a
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2021-08-31 19:46:31 +0200

    no message

--- kafka/entrypoint.sh
+++ kafka/entrypoint.sh
@@ -3,6 +3,7 @@
 export LOG_DIR="${APPDIR}/logs"
 
 sed "s/ZOOKEEPER_HOSTS/${ZOOKEEPER_HOSTS}/g" -i config/server.properties
-sed "s/DATA_DIR/${APPDIR}\/data/g" -i config/server.properties
+sed "s|DATA_DIR|${APPDIR}/data|g" -i config/server.properties
+sed "s/BROKER_ID/${BROKER_ID}/g" -i config/server.properties
 
 kafka-server-start.sh config/server.properties
